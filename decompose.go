9e32a9fc4c873f87eb1ddd97f1acb1e08ebbec3d
Titus Tienaah <titus.intdxdt@gmail.com>
2017-08-23 01:46:03 -0300

    cleanup decompose

--- decompose.go
+++ decompose.go
@@ -10,28 +10,23 @@ const size = 2
 
 //Douglas Peucker decomposition at a given threshold
 func (self *ConstDP) decompose() *deque.Deque {
-	var pln   = self.Pln
-	var hque  = deque.NewDeque()
-	var rg, prg *rng.Range
+	var pln  = self.Pln
+	var hque = deque.NewDeque()
+	var k int
+	var val float64
+	var rg   = pln.Range()
 
-	rg = pln.Range()
-	s := stack.NewStack().Push([size]*rng.Range{rg, prg})
+	s := stack.NewStack().Push(rg)
 
 	for !s.IsEmpty() {
-		ranges  := s.Pop().([size]*rng.Range)
-		rg, prg := ranges[0], ranges[1]
-		k, val  := self.Score(self, rg)
-		//dtn[rg] = (k, val)
-
+		rg = s.Pop().(*rng.Range)
+		k, val  = self.Score(self, rg)
 		if self.is_score_relate_valid(val) {
-			if prg == nil {
-				prg = rg.Clone()
-			}
 			hque.Append(NewHullNode(pln, rg))
 		} else {
 			s.Push(
-				[size]*rng.Range{rng.NewRange(k, rg.J()), rg}, // right
-				[size]*rng.Range{rng.NewRange(rg.I(), k), rg}, // left
+				rng.NewRange(k, rg.J()), // right
+				rng.NewRange(rg.I(), k), // left
 			)
 		}
 	}
